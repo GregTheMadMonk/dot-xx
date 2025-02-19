19eaab8dae82ce2b040c94f56cafe86c3bb54c41
GregTheMadMonk <yagreg7@gmail.com>
2025-02-19 19:55:51 +0300

    Use dxx.cstd.fixed

--- test/main.cc
+++ test/main.cc
@@ -1,9 +1,10 @@
 import std;
 
 import dxx.selftest;
+import dxx.cstd.fixed;
 
 int main(int argc, char** argv) {
     std::unordered_set<std::string> filters;
-    for (std::size_t i = 1; i < argc; ++i) filters.insert(argv[i]);
+    for (uz i = 1; i < argc; ++i) filters.insert(argv[i]);
     return dxx::selftest::run_all(filters);
 }
